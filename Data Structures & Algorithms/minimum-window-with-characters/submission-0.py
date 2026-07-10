95acf4ad9af4efe4eb372c7c6239280fe0c56e6c
Rubisha-09 <rubisharaja@gmail.com>
2026-07-10 21:27:07 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,42 @@
+from typing import List
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+
+        countT = {}
+        window = {}
+
+        for c in t:
+            countT[c] = countT.get(c, 0) + 1
+
+        have = 0
+        need = len(countT)
+        res = [-1, -1]
+        resLen = float("inf")
+
+        left = 0
+
+        for right in range(len(s)):
+            c = s[right]
+            window[c] = window.get(c, 0) + 1
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            while have == need:
+                # Update the minimum window
+                if (right - left + 1) < resLen:
+                    res = [left, right]
+                    resLen = right - left + 1
+
+                # Remove the left character
+                window[s[left]] -= 1
+                if s[left] in countT and window[s[left]] < countT[s[left]]:
+                    have -= 1
+
+                left += 1
+
+        left, right = res
+        return s[left:right + 1] if resLen != float("inf") else ""
